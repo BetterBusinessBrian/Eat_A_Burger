66acb5261b45e597441bd94905ae810a29e88146
BetterBusinessBrian <bman1402@gmail.com>
2020-12-03 10:28:10 -0500

    uploading all directories with backend code

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name)
+VALUES ("Buffalo Blue Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Habenero Cheddar Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Jalepeno Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Burgermeister Meisterburger");
